5f0e8042cbf939dc3b9f22c363a01db7b4b1bdc6
Nookeey <eernestt@o2.pl>
2022-07-08 16:10:21 +0200

    c01

---
+++ stores/authContext.js
@@ -0,0 +1,32 @@
+import { createContext, useState, useEffect } from "react"
+import netlifyIdentity from 'netlify-identity-widget'
+
+const AuthContext = createContext({
+  user: null,
+  login: () => {},
+  logout: () => {},
+  authReady: false
+})
+
+export const AuthContextProvider = ({ children }) => {
+  const [user, serUser] = useState(null)
+
+  useEffect(() => {
+    // init netlify identity connection
+    netlifyIdentity.init()
+  }, [])
+  
+  const login = () => {
+    netlifyIdentity.open()
+  }
+
+  const context = { user, login }
+
+  return (
+    <AuthContext.Provider value={context}>
+      { children }
+    </AuthContext.Provider>
+  )
+}
+
+export default AuthContext
